65a2660e5b54722f059bb7812b67bc6b8da8b37a
jcozzitorto <jcozzitorto@gmail.com>
2018-09-11 14:27:05 -0400

    9-11-18--2

--- CS280/prog0/prog0.cpp
+++ CS280/prog0/prog0.cpp
@@ -14,14 +14,48 @@ int main (int argc, char *argv[]) {
 		}
 
 		else if (s.at(0) == 'L') {
-			for (int j = 1; j < s.length() - 1; j++) {
+			for (int j = 0; j < s.length(); j++) {
 				if (isupper(s.at(j))) {	
 					ch = tolower(s.at(j));
 					cout << ch;
 				}
+
 				else cout << s.at(j);
 			}
+			cout << endl;
 		}
+
+		else if (s.at(0) == 'U') {
+			for (int k = 0; k < s.length(); k++) {
+				if(islower(s.at(k))) {
+					ch = toupper(s.at(k));
+					cout << ch;
+				}
+
+				else cout << s.at(k);
+			}
+			cout << endl;
+		}
+
+		else if (s.at(0) == 'r') {
+			for (int l = s.length() - 1; l > 0; l--) {
+				cout << s.at(l);
+			}
+			cout << endl;
+		}
+
+		else if (s.at(0) == 'R') {
+			for (int m = s.length() - 1; m > 0; m--) {
+				if (islower(s.at(m))) {
+					ch = toupper(s.at(m));
+					cout << ch;
+				}
+
+				else cout << s.at(m);
+			}
+			cout << endl;
+		}
+
 
 		else cout << "FAIL" << endl;
 
