36129d894f1c074f02722169b2869fc7a18a3205
jcozzitorto <jcozzitorto@gmail.com>
2018-10-11 22:14:31 -0400

    10-11-18

--- CS280/prog1/prog1-3.cpp
+++ CS280/prog1/prog1-3.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <map>
 #include <iterator>
+#include <vector>
 
 using namespace std;
 
@@ -35,27 +36,7 @@ int main (int argc, char *argv[]) {
         }
     }
     	
-    	if (argv[2] == NULL) {
-		cout << "NO MODE" << endl;
-		return 0;
-	}
-
-	if (string(argv[2]) == "all") {
-		;
-	}
-
-	else if (string(argv[2]) == "top") {
-		;
-	}
-    
-    else { cout << "INVALID MODE" << endl;
-          return 0;
-         }
-
-	if (argv[3] == NULL)  {
-		cout << "NO FILES GIVEN" << endl;
-        return 0;
-	}
+    	
 	
 	if (atoi(argv[1]) > 0) {
 		for (int i = 3; i < argc; i++) {
@@ -70,27 +51,77 @@ int main (int argc, char *argv[]) {
 			}
 			
 			if (!infile.is_open()) {
-            	cout << "BAD FILE " << s << endl;
-       		}
+            			cout << "BAD FILE " << s << endl;
+       			}
             
-			else infile.close();
-			
-			
+			else infile.close();	
 		}
 		
 			
-       		if (st.length() < (atoi(argv[1])) || (st.empty())){
+		map<string,int>::iterator it;
+				
+		if (string(argv[2]) == "all") {
+			for (it = wordList.begin(); it != wordList.end(); it++) {
+			cout << it -> first << " " << it -> second << endl;
+			}
+		}
+
+		if (string(argv[2]) == "top") {
+			map<int, vector<string>>bycount;
+
+			map<string, int>::iterator it2;
+			for (it2 = wordList.begin(); it2 != wordList.end(); it2++) {
+				bycount[it2 -> second].push_back(it2->first);
+			}
+			
+
+			map<int, vector<string>>::iterator it3;
+				for (it3 = bycount.begin(); it3 != bycount.end(); it3++) {
+					int size = it3 -> second.size();
+			
+					for (int j = 0; j < size; j++) {
+						cout << it3 -> second[j] << " " << it3 -> first << endl;
+					}
+				}
+			
+			map<int, vector<string>>::iterator it4;	
+				for (it4 = bycount.begin(); it4 != bycount.end(); it4++) {
+						int l = 0;
+						int m = 0;
+
+						l = it4 -> first;
+						cout << l;
+				}
+			
+		}
+	
+			
+		else if (string(argv[2]) == "top") {
+			;
+		}		
+
+		else if ((string(argv[2]) != "all") && (string(argv[2]) != "top")) {
+			cout << "INVALID MODE" << endl;
+			return 0;
+		}
+		
+		if (argv[2] == NULL) {
+		cout << "NO MODE" << endl;
+		return 0;
+	}
+
+	if (argv[3] == NULL)  {
+		cout << "NO FILES GIVEN" << endl;
+        return 0;
+	}
+	
+	if (st.length() < (atoi(argv[1])) || (st.empty())){
 					cout << "NO PHRASES" << endl;
 					return 0;
 				}
-				map<string,int>::iterator it;
-				for (it = wordList.begin(); it != wordList.end(); it++) {
-				cout << it -> first << " " << it -> second << endl;
-				}			
-
 			
     		
 	}
-	
-
 }
+
+
