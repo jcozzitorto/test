7260e38161041cdac8b1079500db88d023eee6c4
jcozzitorto <jcozzitorto@gmail.com>
2018-10-16 14:42:35 -0400

    10-16-18--2

--- CS280/prog2/prog2.cpp
+++ CS280/prog2/prog2.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 #include <string>
+#include <istream>
 #include <fstream>
 
 using namespace std;
 
 int main (int argc, char *argv[]) {
 
+	istream *in;
 	ifstream infile;
 
 	for (int i = 0; i < argc; i++) {
-		if (string(argv[i]) == "-v") {
+		string arg = string(argv[i]);
+		
+		if (arg == "-v") {
 			cout << "-v" << endl;
 		}
 
-		else if (string(argv[i]) == "-sum") {
+		else if (arg == "-sum") {
 			cout << "-sum" << endl;
 		}
 
-		else if (string(argv[i]) == "-allids") {
+		else if (arg == "-allids") {
 			cout << "-allids" << endl;
 		}
-
-		else infile.open(string(argv[i]));
-			if (!infile.good()) {
-				cout << "BAD FILE" << endl;
-			}
+		
+		else if ((arg != "-v") && (arg != "sum") && (arg != "-allids")) {
+			infile.open(arg);
+			infile.close();
+		}
+		
+		else in = &cin;
+		
+		
 	}
 }
+
