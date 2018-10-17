c0b7c9e4baa67a8f02c8b919d191839e582a62a7
Jason Cozzitorto <jcozzitorto@gmail.com>
2018-10-17 07:57:37 -0400

    10-17-18

--- CS280/prog2/prog2.cpp
+++ CS280/prog2/prog2.cpp
@@ -7,32 +7,69 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
 
-	istream *in;
+	//istream in;
 	ifstream infile;
+	int fileOpen = 0;
 
-	for (int i = 0; i < argc; i++) {
+
+	for (int i = 1; i < argc; i++) {
 		string arg = string(argv[i]);
-		
+
+		for (int j = 0; j < arg.length(); j++) {
+			char ch;
+			ch = arg.at(j);
+			if ((arg.at(0) == '-') && ((arg.at(1) != 'v') || (arg.at(1) != 's') || (arg.at(1) != 'a'))) {
+				j = arg.length() - 1;
+				cout << "INVALID FLAG ";
+				exit;
+			}
+
+			if ((arg.at(0) == '-') && ((arg.at(1) != 'v') || (arg.at(1) != 's') || (arg.at(1) != 'a'))) {
+				cout << ch;
+			}
+
+		}
+
 		if (arg == "-v") {
-			cout << "-v" << endl;
+			;
 		}
 
 		else if (arg == "-sum") {
-			cout << "-sum" << endl;
+			;
 		}
 
 		else if (arg == "-allids") {
-			cout << "-allids" << endl;
+			;
 		}
-		
-		else if ((arg != "-v") && (arg != "sum") && (arg != "-allids")) {
-			infile.open(arg);
-			infile.close();
+
+
+
+		for (int k = 0; k < arg.length(); k++) {
+			if (arg.at(0) != '-') {
+				if ((arg != "-v") && (arg != "-sum") && (arg != "-allids")) {
+				fileOpen++;
+				infile.open(arg);
+
+				if (fileOpen > 2) {
+				cout << "TOO MANY FILE NAMES" << endl;
+				return 0;
+			}
+
+            if (infile.is_open()) {
+				infile.close();
+			}
+			else {
+				cout << "UNABLE TO OPEN " << arg << endl;
+				return 0;
+			}
+			}
+
+
 		}
-		
-		else in = &cin;
-		
-		
+
+
+
 	}
+}
 }
 
